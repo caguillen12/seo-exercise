3d634ffbc916c182c89acf2cd6285f45213c36ae
Cath Guillen <cmguillen12@gmail.com>
2022-06-20 12:06:39 +0800

    change url

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -5,16 +5,18 @@ const PageLayout = (props) => {
       <>
       <Head>
       <title>Rally</title>
-      <meta name="title" content="Budgeting Made Easy"/>
-      <meta name="description" content="All-in-One Financial Management Tool"></meta>
+      {/* <meta name="title" content="Budgeting Made Easy"/>
+      <meta name="description" content="All-in-One Financial Management Tool"></meta> */}
+      <meta name="viewport" content="width=device-width"/>
+    	<meta charSet="utf-8" />
       <meta property="og:type" content="website"/>
-      <meta property="og:url" content="http://localhost:3000/"/>
+      <meta property="og:url" content="https://seo-exercise-bnl78hdse-caguillen12.vercel.app/"/>
       <meta property="og:title" content="Budgeting Made Easy" key="title" />
       <meta property="og:description" content="All-in-One Financial Management Tool"/>
       <meta property="og:image" content="https://ik.imagekit.io/gclk8b7ii/banner-image_tEQtSDIdi.png?ik-sdk-version=javascript-1.4.3&updatedAt=1655659010257"/>
 
       <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:url" content="http://localhost:3000/"/>
+      <meta property="twitter:url" content="https://seo-exercise-bnl78hdse-caguillen12.vercel.app/"/>
       <meta property="twitter:title" content="Budgeting Made Easy."/>
       <meta property="twitter:description" content="All-in-One Financial Management Tool"/>
       <meta property="twitter:image" content="https://ik.imagekit.io/gclk8b7ii/banner-image_tEQtSDIdi.png?ik-sdk-version=javascript-1.4.3&updatedAt=1655659010257"/>
